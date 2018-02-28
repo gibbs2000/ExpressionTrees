312fb53c5499fd7baf00cce2d198bb932558a5df
Sean Gibbons <gibbonss@bishopireton.org>
2018-02-28 14:45:43 -0500

    b i g    d a b b u

--- ExpressionTrees/src/ExpressionTree.java
+++ ExpressionTrees/src/ExpressionTree.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * Expression Tree
  * 
@@ -14,47 +16,82 @@ public class ExpressionTree extends TreeNode implements Expressions {
 		super(initValue, initLeft, initRight);
 	}
 
-	public ExpressionTree createTree(String[] input) {
-		//Add first element to the center of the tree
-		ExpressionTree exTree = new ExpressionTree(input[0]);
-		
-		return exTree;
+	public ExpressionTree buildTree(String[] exp) {
+		Stack<Object> stk = new Stack<Object>();
+		for (String str : exp) {
+			if (this.isOperation(str)) {
+				TreeNode node = new ExpressionTree(str, new ExpressionTree(stk.pop()), new ExpressionTree(stk.pop()));
+				stk.push(node);
+			} else {
+				stk.push(Integer.parseInt(str));
+			}
+		}
+		return (ExpressionTree) new TreeNode(stk.pop());
 
 	}
 
-	@Override
-	public TreeNode buildTree(String[] exp) {
-		// TODO Auto-generated method stub
-		return null;
+	private boolean isOperation(String s) {
+		if (s.trim().equals("*") || s.trim().equals("+")) {
+			return true;
+		} else
+			return false;
+	}
+
+	private int performOp(int n1, int n2, String operation) {
+		switch (operation.trim()) {
+		case "*":
+			return n1 * n2;
+		case "+":
+			return n1 + n2;
+		default:
+			return n1 + n2;
+		}
 	}
 
 	@Override
 	public int evalTree() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (this.getLeft() == null || this.getRight() == null) {
+			return Integer.parseInt((String) this.getValue());
+		} else {
+			return performOp(((ExpressionTree) this.getLeft()).evalTree(),
+					((ExpressionTree) this.getRight()).evalTree(), (String) this.getValue());
+		}
 	}
 
 	@Override
 	public String toPrefixNotation() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.getLeft() == null || this.getRight() == null) {
+			return (String) this.getValue();
+		} else {
+			return (String) this.getValue() + (String) ((ExpressionTree) this.getLeft()).toPrefixNotation()
+					+ (String) ((ExpressionTree) this.getRight()).toPrefixNotation();
+		}
 	}
 
 	@Override
 	public String toInfixNotation() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.getLeft() == null || this.getRight() == null) {
+			return (String) this.getValue();
+		} else {
+			return "(" + (String) ((ExpressionTree) this.getLeft()).toPrefixNotation() + (String) this.getValue()
+					+ (String) ((ExpressionTree) this.getRight()).toPrefixNotation() + ")";
+		}
 	}
 
 	@Override
 	public String toPostfixNotation() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.getLeft() == null || this.getRight() == null) {
+			return (String) this.getValue();
+		} else {
+			return (String) ((ExpressionTree) this.getLeft()).toPostfixNotation()
+					+ (String) ((ExpressionTree) this.getRight()).toPostfixNotation() + (String) this.getValue();
+		}
 	}
 
 	@Override
 	public int postfixEval(String[] exp) {
-		// TODO Auto-generated method stub
-		return 0;
+		TreeNode tree = new ExpressionTree("str");
+		tree = ((ExpressionTree) tree).buildTree(exp);
+		return ((ExpressionTree) tree).evalTree();
 	}
 }
